8b8e0d7743bfdc5f006ea08dc2dc01bc81209283
saisubham-29 <saisubhamtablist@gmail.com>
2026-02-02 22:40:13 +0530

    Done up to basic image and relative layout.

--- app/src/main/java/com/example/myresume/aboutMe.java
+++ app/src/main/java/com/example/myresume/aboutMe.java
@@ -1,8 +1,6 @@
 package com.example.myresume;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn;
+public class aboutMe extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_me);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btn=findViewById(R.id.AboutMeBtn);
-        btn.setOnClickListener(view -> {
-        Intent intent =
-        });
-
     }
 }
